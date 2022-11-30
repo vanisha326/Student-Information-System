88ef0398286f86820fa395b3b584a4ae044b5f08
Vanisha <garg.vanisha11@gmail.com>
2022-11-30 20:43:22 +0530

    Initial commit

---
+++ src/MyFunction.java
@@ -0,0 +1,38 @@
+
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author shrig
+ */
+public class MyFunction {
+    public static int countData(String tableName)
+    {
+        int total =0;
+        Connection con = MyConnection.getConnection();
+        Statement st;
+        try {
+            st = con.createStatement();
+             ResultSet rs = st.executeQuery("SELECT COUNT(*) AS 'total' FROM "+tableName);
+             while(rs.next()){
+                 total = rs.getInt(1);
+             }
+        } catch (SQLException ex) {
+            Logger.getLogger(MyFunction.class.getName()).log(Level.SEVERE, null, ex);
+        }
+       
+        return total;
+    }
+    
+}
